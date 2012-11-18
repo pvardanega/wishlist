01bd04a1ee5e1c0ce3996b250a16bf873c5f8aa5
Pierre-Jean Vardanéga <pierrejean.vardanega@gmail.com>
2012-11-18 15:17:35 +0100

    Add an admin account if no account exists

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,8 +7,8 @@ class BootStrap {
     def init = { servletContext ->
         environments {
             development {
-                def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-                def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+                def adminRole = new Role(authority: Role.ROLE_ADMIN).save(flush: true)
+                def userRole = new Role(authority: Role.ROLE_USER).save(flush: true)
 
                 def adminUser = new User(username: 'admin', email: 'admin@yopmail.com',
                         firstname: 'firstname', lastname: 'lastname', enabled: true,
@@ -22,6 +22,20 @@ class BootStrap {
 
                 UserRole.create adminUser, adminRole, true
                 UserRole.create user, userRole, true
+            }
+            production {
+                if (!Role.findByAuthority(Role.ROLE_ADMIN)) {
+                    def adminRole = new Role(authority: Role.ROLE_ADMIN).save(flush: true)
+                    if (!UserRole.findByRole(adminRole)) {
+                        def adminUser = new User(username: 'admin', email: 'admin@yopmail.com',
+                                firstname: 'firstname', lastname: 'lastname', enabled: true,
+                                password: 'password')
+                        adminUser.save(flush: true)
+                    }
+                }
+                if (!Role.findByAuthority(Role.ROLE_USER)) {
+                    new Role(authority: Role.ROLE_USER).save(flush: true)
+                }
             }
         }
     }
--- grails-app/domain/com/pvardanega/Role.groovy
+++ grails-app/domain/com/pvardanega/Role.groovy
@@ -2,6 +2,9 @@ package com.pvardanega
 
 class Role {
 
+    public final static String ROLE_ADMIN = 'ROLE_ADMIN'
+    public final static String ROLE_USER = 'ROLE_USER'
+
 	String authority
 
 	static mapping = {
