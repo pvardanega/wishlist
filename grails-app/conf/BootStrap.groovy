88ed42220ff5524549edd52af48f8711147935de
Pierre-Jean Vardanéga <pierrejean.vardanega@gmail.com>
2012-11-23 23:06:59 +0100

    Update boostrap configuration

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,18 +10,18 @@ class BootStrap {
                 def adminRole = new Role(authority: Role.ROLE_ADMIN).save(flush: true)
                 def userRole = new Role(authority: Role.ROLE_USER).save(flush: true)
 
-                def adminUser = new User(username: 'admin', email: 'admin@yopmail.com',
-                        firstname: 'firstname', lastname: 'lastname', enabled: true,
-                        password: 'password')
-                adminUser.save(flush: true)
+                def pj = new User(username: 'Pierre-Jean V.', email: 'pj@yopmail.com', firstname: 'Pierre-Jean',
+                        lastname: 'Vardanega', enabled: true, password: 'password').save(flush: true)
 
-                def user = new User(username: 'user', email: 'user@yopmail.com',
-                        firstname: 'firstname', lastname: 'lastname', enabled: true,
-                        password: 'password')
-                user.save(flush: true)
+                def amira = new User(username: 'Amira L.', email: 'amira@yopmail.com', firstname: 'Amira',
+                        lastname: 'Lakhal', enabled: true, password: 'password').save(flush: true)
 
-                UserRole.create adminUser, adminRole, true
-                UserRole.create user, userRole, true
+                def baptiste = new User(username: 'Baptiste V.', email: 'baptiste@yopmail.com', firstname: 'Baptiste',
+                        lastname: 'Vardanega', enabled: true, password: 'password').save(flush: true)
+
+                UserRole.create pj, adminRole, true
+                UserRole.create amira, userRole, true
+                UserRole.create baptiste, userRole, true
             }
             production {
                 def adminRole = Role.findByAuthority(Role.ROLE_ADMIN)
